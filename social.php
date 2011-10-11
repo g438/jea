70ada9d4bcbecb1df8f12887f0f6ff24ea3bbc46
Sylvain Philip <contact@sphilip.com>
2011-10-11 10:16:34 +0000

    [#26949] Apply patch on Roberto Segura social plugin

--- social.php
+++ social.php
@@ -1,10 +1,10 @@
 <?php
 /**
  *
- * @package		Joomla
- * @subpackage	JEA
- * @copyright	Copyright (C) 2011 PHILIP Sylvain. All rights reserved.
- * @license		GNU/GPL, see LICENSE.txt
+ * @package        Joomla
+ * @subpackage    JEA
+ * @copyright    Copyright (C) 2011 Roberto Segura. All rights reserved.
+ * @license        GNU/GPL, see LICENSE.txt
  * Joomla Estate Agency is free software. This version may have been modified pursuant to the
  * GNU General Public License, and as distributed it includes or is derivative
  * of works licensed under the GNU General Public License or other free or open
@@ -18,101 +18,38 @@ jimport('joomla.event.plugin');
 JPlugin::loadLanguage('plg_jea_fields', JPATH_ADMINISTRATOR);
 
 /**
- * Example JEA Plugin
+ * Plugin JEA Social
  *
- * @package		Joomla
- * @subpackage	JEA
- * @since 		1.5
+ * @package        Joomla
+ * @subpackage    JEA
+ * @since         1.5
  */
 class plgJeaSocial extends JPlugin
 {
     var $_params = null;
     var $_position = 1;
 
-	/**
-	 * Constructor
-	 *
-	 * For php4 compatability we must not use the __constructor as a constructor for plugins
-	 * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
-	 * This causes problems with cross-referencing necessary for the observer design pattern.
-	 *
-	 * @param object $subject The object to observe
-	 * @param object $params  The object that holds the plugin parameters
-	 * @since 1.5
-	 */
-	function plgJeaSocial( &$subject, $params )
-	{
-		parent::__construct( $subject, $params );
-		global $mainframe;
-		$plugin =& JPluginHelper::getPlugin('jea', 'social');
-	    $like = '';  $retweet = '';  $buzz = '';  $digg = '';
-    	$this->_params = new JParameter( $plugin->params );
-    	$this->_position = ($this->_params->get('position'));
-	}
-
-
     /**
-     * onInitTableProperty Method
-     * This method is called just before $row property initialization.
-     * It should be usefull to add fields on the fly
+     * Constructor
      *
-     * @param TableProperties $table
-     * @return void;
+     * For php4 compatability we must not use the __constructor as a constructor for plugins
+     * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
+     * This causes problems with cross-referencing necessary for the observer design pattern.
+     *
+     * @param object $subject The object to observe
+     * @param object $params  The object that holds the plugin parameters
+     * @since 1.5
      */
-    function onInitTableProperty(&$table)
+    function plgJeaSocial( &$subject, $params )
     {
-
+        parent::__construct( $subject, $params );
+        global $mainframe;
+        $plugin =& JPluginHelper::getPlugin('jea', 'social');
+        $like = '';  $retweet = '';  $buzz = '';  $digg = '';
+        $this->_params = new JParameter( $plugin->params );
+        $this->_position = ($this->_params->get('position'));
     }
 
-    /**
-     * onBeforeSaveProperty method
-     *
-     * Called before $row property save.
-     *
-     * @param TableProperties $row
-     * @param boolean $is_new
-     * @return boolean
-     */
-    function onBeforeSaveProperty(&$row, $is_new)
-    {
-
-    }
-
-	/**
-     * onAfterSaveProperty method
-     *
-     * Called after $row property save.
-     *
-     * @param TableProperties $row
-     * @param boolean $is_new
-     * @return void
-     */
-    function onAfterSaveProperty(&$row, $is_new)
-    {
-
-    }
-
-	/**
-     * onAfterStartPane method (Called in the admin property form)
-     *
-     * @param JPane $pane
-     * @param TableProperties $row
-     */
-    function onAfterStartPane(&$pane, &$row)
-    {
-
-    }
-
-    /**
-     * onBeforeEndPane method (Called in the admin property form)
-     *
-     * @param JPane $pane
-     * @param TableProperties $row
-     */
-    function onBeforeEndPane(&$pane, &$row)
-    {
-
-    }
 
 
     /**
@@ -124,12 +61,12 @@ class plgJeaSocial extends JPlugin
     function onBeforeShowDescription(&$row)
     {
         if($this->_position == 1 || $this->_position == 2) {
-            $this->showSocialBar($row);            
+            $this->showSocialBar($row);
         }
     }
 
 
-	/**
+    /**
      * onAfterShowDescription method
      * Called in the default_item.php tpl
      *
@@ -138,37 +75,37 @@ class plgJeaSocial extends JPlugin
     function onAfterShowDescription(&$row)
     {
         if($this->_position == 0 || $this->_position == 2) {
-            $this->showSocialBar($row);            
+            $this->showSocialBar($row);
         }
     }
 
 
     function showSocialBar(&$row) {
+
         // ensure that constructor got the plugin params
-        if(!is_null($this->_params)) {
+        if (!is_null($this->_params) && !empty($row->id)) {
             // initialize empty vars
             $like = '';  $retweet = '';  $buzz = '';  $digg = '';
-            // ensure that we are viewing a property
-        	if(JRequest::getVar('view','')=='properties' && isset($row->id)) {
-        	    // build the property url
-                if(isset($row->id)) $url = "http://".$_SERVER['HTTP_HOST'].JRoute::_('index.php?option=com_jea&view=properties&id='.$row->slug);
-            }
-            else return;
+
+            // build the property url
+            $uri =& JFactory::getURI();
+            $url = $uri->toString(array('scheme','host', 'port')).JRoute::_('index.php?option=com_jea&view=properties&id='.$row->slug);
+
             $url2 = urlencode($url);
             // facebook             
-            if($this->_params->get('like')) {
+            if ($this->_params->get('like')) {
                 $like = '<div class="faceandtweet_like" style="float:'.$this->_params->get('float').'; width:'.$this->_params->get('like_width').'px; height:'.$this->_params->get('like_height').'px;"><iframe src="http://www.facebook.com/plugins/like.php?href='.$url2 .'&amp;layout='.$this->_params->get('like_style').'&amp;width='.$this->_params->get('like_width').'&amp;show_faces=false&amp;action='.$this->_params->get('like_verb').'&amp;colorscheme='.$this->_params->get('like_color_scheme').'&amp;height='.$this->_params->get('like_height').'" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:'.$this->_params->get('like_width').'px; height:'.$this->_params->get('like_height').'px;" allowTransparency="true"></iframe></div>';                
             }
             // twitter
-            if($this->_params->get('retweet')) {
+            if ($this->_params->get('retweet')) {
                 $retweet = '<div class="faceandtweet_retweet" style="float:'.$this->_params->get('float').'; width:'.$this->_params->get('count-width').'px;"><a href="http://twitter.com/share?url='.$url2.'" class="twitter-share-button" data-text="'.$row->title.':" data-count="'.$this->_params->get('count').'" data-via="'.$this->_params->get('twitter_account').'" data-related="'.$this->_params->get('twitter_account2').'">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></div>';
-            }	
+            }
             // buzz
-            if($this->_params->get('buzz')) {
+            if ($this->_params->get('buzz')) {
                 $buzz = '<div class="faceandtweet_retweet" style="float:'.$this->_params->get('float').'; width:110px;"><a title="'.$row->title.'" class="google-buzz-button" href="http://www.google.com/buzz/post" data-button-style="small-count"></a><script type="text/javascript" src="http://www.google.com/buzz/api/button.js"></script></div>';  
             }
-            // digg		
-            if($this->_params->get('digg')) {
+            // digg
+            if ($this->_params->get('digg')) {
                 $digg = '<div class="faceandtweet_retweet" style="float:'.$this->_params->get('float').'; width:110px;">';
                 $digg .= '<script type="text/javascript">
                                 (function() {
@@ -186,8 +123,7 @@ class plgJeaSocial extends JPlugin
             $bar = $retweet . $like . $buzz . $digg;
             // format content
             $bar = '<div class="faceandtweet">'.$bar.'<div style="clear:both;"></div></div>';
-		    echo $bar;
+            echo $bar;
         } 
     }
-
 }
